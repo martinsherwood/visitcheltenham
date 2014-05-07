94d774ad491a63ac00feb4ce701b6a566731ec51
Martin Sherwood <martin@jsherwood.com>
2014-05-07 23:06:23 +0100

    update constants.php, removed passwords etc

--- server/includes/constants.php
+++ server/includes/constants.php
@@ -1,5 +1,5 @@
 <?php
 	define("DB_SERVER", "localhost");
-	define("DB_USER", "martinsh_1");
-	define("DB_PASSWORD", "?CpqI8_Gac2=");
-	define("DB_NAME", "martinsh_visitcheltenham");
+	define("DB_USER", "");
+	define("DB_PASSWORD", "");
+	define("DB_NAME", "");
